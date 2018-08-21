340d7f305ffeb387bbeef43a63e97d502847c0b8
Nick Jackson <njackson2012@gmail.com>
2018-08-20 19:08:54 -0700

    Make php connect to database

--- phpStuff/helloWorld.php
+++ phpStuff/helloWorld.php
@@ -3,6 +3,19 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php
+	$servername = "den1.mysql4.gear.host";
+	$username = "nixondb";
+	$password = "Cy5G?_5x09e9";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully";
+	?>
  </body>
 </html>
