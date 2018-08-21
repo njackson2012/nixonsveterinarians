7752a4275ee037ab46ec0f890319e9c695aec7e7
Nick Jackson <njackson2012@gmail.com>
2018-08-20 18:39:56 -0700

    Add MySql.Data.dll to all teh places

--- CheckersGame/Assets/Scripts/Client.cs
+++ CheckersGame/Assets/Scripts/Client.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
-public class Client : MonoBehaviour {
+public class Client : MonoBehaviour
+{
     private string username = "nixondb";
     private string password = "Cy5G?_5x09e9";
     private string hostname = "den1.mysql4.gear.host";
     private string connectionString;
     private MySqlConnection DBConn = null;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         connectionString = "UID=" + username + ";DATABASE=" + hostname + ";PWD=" + password + ";";
         DBConn = new MySqlConnection(connectionString);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
