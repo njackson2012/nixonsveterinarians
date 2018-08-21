88adbe5bc3a5d7ca2c76ccc25cc0bfd293d789ac
Nick Jackson <njackson2012@gmail.com>
2018-08-20 20:05:51 -0700

    Add SQL query to php

--- phpStuff/helloWorld.php
+++ phpStuff/helloWorld.php
@@ -7,15 +7,20 @@
 	$servername = "den1.mysql4.gear.host";
 	$username = "nixondb";
 	$password = "Cy5G?_5x09e9";
+	$dbname = "nixondb";
 
 	// Create connection
-	$conn = new mysqli($servername, $username, $password);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
-	} 
+	}
+	$sql = "SELECT * FROM games";
+	$result = $conn->query($sql);
 	echo "Connected successfully";
+	echo $result;
+	$conn->close();
 	?>
  </body>
 </html>
