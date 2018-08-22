090a5c9b8727d72256b8a0b69c88643f4e9ea5f3
Nick Jackson <njackson2012@gmail.com>
2018-08-22 13:12:53 -0700

    Add new client based on PHP

--- CheckersGame/Assets/Scripts/Client.cs
+++ CheckersGame/Assets/Scripts/Client.cs
@@ -1,21 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
-using MySql.Data;
-using MySql.Data.MySqlClient;
 
 public class Client : MonoBehaviour
 {
-    private string username = "nixondb";
-    private string password = "Cy5G?_5x09e9";
-    private string hostname = "den1.mysql4.gear.host";
-    private string connectionString;
-    private MySqlConnection DBConn = null;
+    // PHP endpoint URLS for Game management
+    public string insertGameURL = "http://nixonphpconnector.gearhostpreview.com/phpStuff/insertGame.php?";
+    public string selectGameURL = "http://nixonphpconnector.gearhostpreview.com/phpStuff/selectGame.php?";
+    public string deleteGameURL = "http://nixonphpconnector.gearhostpreview.com/phpStuff/deleteGame.php?";
+
+    // PHP endpoint URLs for Piece management
+    public string insertPieceURL = "http://nixonphpconnector.gearhostpreview.com/phpStuff/insertPiece.php?";
+    public string selectPieceURL = "http://nixonphpconnector.gearhostpreview.com/phpStuff/selectPiece.php?";
+    public string deletePieceURL = "http://nixonphpconnector.gearhostpreview.com/phpStuff/deletePiece.php?";
+
+    public string helloWorldURL = "http://nixonphpconnector.gearhostpreview.com/phpStuff/helloWorld.php";
     // Use this for initialization
     void Start()
     {
-        connectionString = "UID=" + username + ";DATABASE=" + hostname + ";PWD=" + password + ";";
-        DBConn = new MySqlConnection(connectionString);
+        string selectResult = getGame(1);
+        print(selectResult);
+        selectResult = listDatbase();
+        string[] words = selectResult.Split(' ');
+        foreach (string word in words)
+        {
+            print(word);
+        }
+        print(selectResult);
+    }
+
+    string getGame(int ID)
+    {
+        WWW selectResult = new WWW(selectGameURL + "GAMEID=" + ID);
+        while(! selectResult.isDone)
+        {
+            Thread.Sleep(100);
+        }
+        return selectResult.text;
+    }
+
+    string listDatbase()
+    {
+        WWW results = new WWW(helloWorldURL);
+        while(! results.isDone)
+        {
+            Thread.Sleep(100);
+        }
+        return results.text;
     }
 
     // Update is called once per frame
