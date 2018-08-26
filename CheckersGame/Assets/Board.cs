a5bd389d7965ea34730f68a35aa0d5ea50ebc17f
Joseph3435 <30558803+Joseph3435@users.noreply.github.com>
2018-08-26 11:39:09 -0400

    reverting back to nicks commit

--- CheckersGame/Assets/Board.cs
+++ CheckersGame/Assets/Board.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class Board : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
