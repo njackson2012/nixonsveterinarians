e6c0510eb91320643e9b7ceccc31219cf339874c
Robert Ross <rossrd@gmail.com>
2018-08-26 10:43:11 -0400

    added attributes, getters, and setters to Board and Piece classes.  Started the GameController class.

---
+++ CheckersGame/Assets/Scripts/Board.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private Piece[] pieces = new Piece[24];
+
+    public GameObject redPiecePreFab;
+
+    public GameObject blackPiecePreFab;
+
+    //some parameter defined by Nick - what does that database need from us to know which game to get the piece array from?
+    public Piece[] GetPieces()
+    {
+        return pieces;
+    }
+
+    public void RemovePiece(int pieceId)
+    {
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            if (pieces[i].GetId() == pieceId)
+            {
+                pieces[i].SetLocation(new int[] {-1,-1});
+            }
+        }
+    }
+    public void GenerateBoard()
+    {
+        //Generate Red Pieces
+
+        //Generate Black Pieces
+
+    }
+	// Use this for initialization
+	public void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+}
